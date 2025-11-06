726a1187111ee3f9df2f07a1d6b9e538184dd624
yurimanhica-dev <yurimanhica4@gmail.com>
2025-11-06 17:14:35 +0200

    fe

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -33,49 +33,15 @@ interface Project {
 }
 
 export const projects: Project[] = [
-  // {
-  //   name: "MALEsoftware Dashboard",
-  //   image: "/images/malesoftware.png",
-  //   link: "https://malesoftware.co.mz",
-  //   github: "https://github.com/username/malesoftware",
-  //   description:
-  //     "Dashboard moderno e responsivo para gestão financeira e relatórios em tempo real.",
-  //   tech: ["Next.js", "TypeScript", "TailwindCSS", "Framer Motion"],
-  // },
-  // {
-  //   name: "Website MALEcaixa",
-  //   image: "/projects/malecaixa.png",
-  //   link: "https://malecaixa.vercel.app/",
-  // },
-  // {
-  //   name: "Website MALEseguros",
-  //   image: "/projects/maleseguros.png",
-  //   link: "https://maleseguros.vercel.app/",
-  // },
-  // {
-  //   name: "Website MALEholding (beta)",
-  //   image: "/projects/maleholding.png",
-  //   link: "https://maleholding.vercel.app/",
-  // },
-  // {
-  //   name: "Website MALEsoftware (beta)",
-  //   image: "/projects/malesoftware.png",
-  //   link: "https://malesoftware.vercel.app/",
-  // },
-  // {
-  //   name: "Amigo Oculto Online",
-  //   image: "/projects/secret_friend.png",
-  //   link: "https://secretfriends.vercel.app/",
-  // },
-  // {
-  //   name: "MALEsoftware (demo)",
-  //   image: "/projects/malesoftware.png",
-  //   link: "https://malesoftware.vercel.app",
-  //   github: "https://github.com/yurimanhica-dev/malesoftware_tech",
-  //   description:
-  //     "Website institucional para empresa de software, apresentando serviços e portfólio.",
-  //   tech: ["Vite", "TypeScript", "TailwindCSS", "Framer Motion"],
-  // },
+  {
+    name: "Modulo de Recursos Humanos",
+    image: "/projects/MRH.png",
+    link: "https://malesoftware.vercel.app",
+    github: "https://github.com/yurimanhica-dev/malesoftware_tech",
+    description:
+      "Modulo de RH para sistema de gestão empresarial, facilitando a administração de funcionários e processos de pagamento.",
+    tech: ["C#", "ASP.NET", "Windows Forms", "WPF"],
+  },
   {
     name: "MALEcaixa",
     image: "/projects/malecaixa.png",
